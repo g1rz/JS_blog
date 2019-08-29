e47aa9026c1376dfbd25ee9449bd9c5dc19a97fc
Sergey Peshkov <zloi.g1rz@gmail.com>
2019-08-29 15:59:40 +0800

    Get posts from Firebase

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,21 +1,35 @@
 class ApiService {
-  constructor(baseUrl) {
-    this.url = baseUrl;
-  }
-
-  async createPost(post) {
-    try {
-      const request = new Request(this.url + '/posts.json', {
-        method: 'post',
-        body: JSON.stringify(post)
-      });
-      const response = await fetch(request);
-      return await response.json();
-    } catch(error) {
-      console.error(error);
+    constructor(baseUrl) {
+        this.url = baseUrl;
     }
-    
-  }
+
+    async createPost(post) {
+        try {
+            const request = new Request(this.url + '/posts.json', {
+                method: 'post',
+                body: JSON.stringify(post)
+            });
+            return this.useRequest(request);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    async fetchPosts() {
+        try {
+            const request = new Request(`${this.url}/posts.json`, {
+                method: 'get'
+            });
+            return this.useRequest(request);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    async useRequest(request) {
+        const response = await fetch(request);
+        return await response.json();
+    }
 }
 
 export const apiService = new ApiService('https://g1rz-js-blog.firebaseio.com');
