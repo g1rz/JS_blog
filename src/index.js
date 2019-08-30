bb9693f54e1ae24652af59b57016a10a5e28ead2
Sergey Peshkov <zloi.g1rz@gmail.com>
2019-08-30 16:25:38 +0800

    Finish application

--- src/index.js
+++ src/index.js
@@ -11,9 +11,9 @@ const navigation = new NavigationComponent('navigation');
 
 const loader = new LoaderComponent('loader');
 
-const posts = new PostsComponent('posts', {loader});
+const posts = new PostsComponent('posts', { loader });
 const create = new CreateComponent('create');
-const favorite = new FavoriteComponent('favorite');
+const favorite = new FavoriteComponent('favorite', { loader });
 
 navigation.registerTabs([
     { name: 'create', component: create },
